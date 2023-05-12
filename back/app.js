b96c4b0d123de88472809c212c19fde81d20a09d
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-05-12 18:01:10 +0200

    modification of the app.js

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
+const helmet = require("helmet");
 
 const saucesRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
@@ -15,11 +16,13 @@ mongoose.connect(process.env.MONGO_URI,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
+app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site")
   next();
 });
 
