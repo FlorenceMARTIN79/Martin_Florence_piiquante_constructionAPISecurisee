9a13d3f0cf35645c2a033236de431079e1917fc9
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-05-13 22:42:20 +0200

    improves the layer of the files

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,16 +1,11 @@
-//Importations
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// Middlewares d'authentification
-
 exports.signup = (req, res, next) => {
-  //hasher le mdp + nbr de tour de hashage
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
-      // nouvel utilisateur, enregistrement dans la bdd
       const user = new User({
         email: req.body.email,
         password: hash,
@@ -26,19 +21,15 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
-      // vérifier que l'utilisateur existe
       if (!user) {
         return res.status(401).json({ error: "Utilisateur non trouvé!" });
       }
-      // si oui, vérifier le mdp
       bcrypt
         .compare(req.body.password, user.password)
         .then((valid) => {
-          // si comparaion fausse
           if (!valid) {
             return res.status(401).json({ error: "Mot de passe incorrect!" });
           }
-          // comparaison true
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
--- back/routes/sauces.js
+++ back/routes/sauces.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-const saucesCtrl = require('../controllers/sauce');
+const saucesCtrl = require("../controllers/sauce");
 
 router.post("/", auth, multer, saucesCtrl.createSauce);
 
